f5a24db74d062b2d8a42c4a3c26cacba43e65561
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-10 01:22:01 +0530

    add the_twin_monuments.cpp

---
+++ the_twin_monuments.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> res;
+
+vector<pair<int, int>> findCells(int x, int y, int a, int b)
+{
+    vector<pair<int, int>> possibleCells;
+    vector<pair<int, int>> moves = {
+        {a, b}, {a, -b}, {-a, b}, {-a, -b}, {b, a}, {b, -a}, {-b, a}, {-b, -a}};
+
+    for (auto move : moves)
+    {
+        possibleCells.push_back({x + move.first, y + move.second});
+    }
+    return possibleCells;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, x1, y1, x2, y2, k = 0;
+        cin >> a >> b >> x1 >> y1 >> x2 >> y2;
+
+        auto possibleCells1 = findCells(x1, y1, a, b);
+        auto possibleCells2 = findCells(x2, y2, a, b);
+
+        set<pair<int, int>> cellsSet1(possibleCells1.begin(), possibleCells1.end());
+        for (auto cell : possibleCells2)
+        {
+            if (cellsSet1.count(cell))
+            {
+                k++;
+            }
+        }
+        res.push_back(k);
+    }
+    for (int r : res)
+    {
+        cout << r << endl;
+    }
+}
